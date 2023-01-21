0c5b46e1468c491bc3808e3aa42c7bbfe89230dc
bilalbentoumi <bilalbentoumi@gmail.com>
2023-01-21 16:02:11 +0100

    Cleanup project

--- public/js/validation.en.js
+++
@@ -1,8 +0,0 @@
-var messages = {
-    'required': 'this field is required',
-    'email': 'please enter a valid email address',
-    'equalTo': 'not match',
-    'min': 'this field must contain at least',
-    'max': 'this field must contain at most',
-    'characters': 'characters',
-};
